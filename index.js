98f91c57b1e0734215efcd38d388d5a9ad6b64c5
James Sinkala <xinnks@gmail.com>
2022-05-05 00:12:46 +0300

    feat: Add response body analyzing function

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
 
 /**
+ * readRequestBody reads in the incoming request body
+ * Use await readRequestBody(..) in an async function to get the string
+ * @param {Request} request the incoming request to read from
+ */
+ async function readRequestBody(request) {
+  const { headers } = request;
+  const contentType = headers.get('content-type') || '';
+
+  if (contentType.includes('application/json')) {
+    return await request.json();
+  } else if (contentType.includes('form')) {
+    const formData = await request.formData();
+    let body = {};
+    for (const entry of formData.entries()) {
+      body[entry[0]] = entry[1];
+    }
+    return body;
+  } else {
+    return null;
+  }
+}
+
+/**
  * Returns email Html markup
  * @param {Object} data => email data object
  */
